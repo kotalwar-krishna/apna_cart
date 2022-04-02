10228ae382de3b99a1bd708fefb3ad4257471abd
Aditya_kotalwar <63541618+kotalwar-krishna@users.noreply.github.com>
2022-04-02 09:58:37 +0530

    commit

---
+++ src/components/ProductList.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+import Product from './Product'
+
+export default function ProductList(props) {
+    // console.log(props);
+  return (
+    props.productList.length > 0 ?
+    props.productList.map((product,i)=>{
+        return <Product product={product} key={i} incrementQuantity={props.incrementQuantity} index={i} decrementQuantity={props.decrementQuantity} removeItem={props.removeItem}/>
+    }) : <h1>Cart is Empty</h1>
+  )
+}
